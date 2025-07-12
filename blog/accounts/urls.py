605eea13bfe534c24028ccca1206887dde3b7648
alainkatayi <alainkatayi01@gmail.com>
2025-07-12 23:32:42 +0100

    création de la route users dans le fichier des urls de accounts, qui return la lste des utilsateurs

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RegisterView
+from .views import UserList
 
 urlpatterns=[
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('users/', UserList.as_view(), name='user')
 ]
