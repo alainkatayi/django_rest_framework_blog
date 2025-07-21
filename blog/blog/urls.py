746771e0e6a5cecea960c475deefe4e9f1c830cd
alainkatayi <alainkatayi01@gmail.com>
2025-07-21 18:13:21 +0100

    Ajout de l'url api/article dans le fichier des urls principal

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -21,6 +21,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
+    path('api/article/', include('articles.urls')),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # login
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # refresh
