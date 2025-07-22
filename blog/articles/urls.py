755d5eb2c583fce3a211d7ec616a45ab69e0fb7a
alainkatayi <alainkatayi01@gmail.com>
2025-07-22 19:50:12 +0100

    Implémentation de la vue ArticleUniqueView dans le fichier des vuesm pour voir un article en commentaire

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ArticleCreatedView, ArticleList,ArticleUpdateView, ArticleDeleteView
+from .views import ArticleCreatedView, ArticleList,ArticleUpdateView, ArticleDeleteView,ArticleUniqueView
 urlpatterns = [
-    path('create/',ArticleCreatedView.as_view(), name='create' ),
-    path('list/',ArticleList.as_view(), name='listArticle' ),
-    path('<int:pk>/update/',ArticleUpdateView.as_view(), name='article-update'),
-    path('<int:pk>/delete/',ArticleDeleteView.as_view(), name='delete_article')
+    path('create/',ArticleCreatedView.as_view(), name='create' ),#http://127.0.0.1:8000/api/article/create/
+    path('list/',ArticleList.as_view(), name='listArticle' ), #http://127.0.0.1:8000/api/article/list/
+    path('<int:pk>/update/',ArticleUpdateView.as_view(), name='article-update'), #http://127.0.0.1:8000/api/article/1/update/
+    path('<int:pk>/delete/',ArticleDeleteView.as_view(), name='delete_article'), #http://127.0.0.1:8000/api/article/1/delete/
+    path('<int:pk>/',ArticleUniqueView.as_view(), name='unique') #http://127.0.0.1:8000/api/article/1
 ]
