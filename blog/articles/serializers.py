83fef37bd6bb23aecc1cc904e948ef6fa7307439
alainkatayi <alainkatayi01@gmail.com>
2025-08-06 13:22:39 +0100

    Ajout du champ introduction dans le serializers du Model Article

--- blog/articles/serializers.py
+++ blog/articles/serializers.py
@@ -4,10 +4,11 @@ from accounts.serializers import UserPublicSerializer
 
 class ArticleSerializer(serializers.ModelSerializer):
     title=  serializers.CharField(required=True)
+    introduction = serializers.CharField(required=True)
     content =serializers.CharField(required=True)
     created_by = UserPublicSerializer(read_only=True)
     class Meta:
         model = Article
-        fields = ['id','title','content','created_at', 'created_by']
+        fields = ['id','title','introduction','content','created_at', 'created_by']
         read_only_fields = ['created_by','created_at']
 
