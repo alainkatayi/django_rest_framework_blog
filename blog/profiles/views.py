37639b400084d650f97c3f79972b13cff5879e56
alainkatayi <alainkatayi01@gmail.com>
2025-08-09 00:03:32 +0100

    création de la class SkillsListView dans le fichier view.py de l'application profiles, qui return la liste de tous les skills

--- blog/profiles/views.py
+++ blog/profiles/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import SkillsSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Skills
 
 # Create your views here.
 class CreateSkillView(APIView):
@@ -17,3 +19,9 @@ class CreateSkillView(APIView):
                 "skill": SkillsSerializer(skill).data
             },status= status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class SkillsListView(ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Skills.objects.all()
+    serializer_class = SkillsSerializer
+    
