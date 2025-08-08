3f5838ec3e111a79a82c7295af04aea2f97e8912
alainkatayi <alainkatayi01@gmail.com>
2025-08-09 00:05:32 +0100

    Création de la route api/profile/skills dans le fichier urls.py de l'application profiles, qui return la liste de tous les skills

--- blog/profiles/urls.py
+++ blog/profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateSkillView
+from .views import CreateSkillView, SkillsListView
 
 urlpatterns = [
-    path('create/skill/',CreateSkillView.as_view(), name='create')
+    path('create/skill/',CreateSkillView.as_view(), name='create'),
+    path('skills/',SkillsListView.as_view(), name='skills-list')
 ]
