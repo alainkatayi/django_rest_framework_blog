1068fcb9a12315b836b6cc60666df983cbc1d735
alainkatayi <alainkatayi01@gmail.com>
2025-08-09 11:05:31 +0100

    Implémentation de la View SkillDeletedView, qui supprime une compétence, dans le fichier View.py de l'application profiles

--- blog/profiles/urls.py
+++ blog/profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateSkillView, SkillsListView,SkillUpdateView
+from .views import CreateSkillView, SkillsListView,SkillUpdateView,SkillDeletedView
 
 urlpatterns = [
     path('create/skill/',CreateSkillView.as_view(), name='create'),
     path('skills/',SkillsListView.as_view(), name='skills-list'),
     path('<int:pk>/update/',SkillUpdateView.as_view(), name='update-skill'),
+    path('<int:pk>/delete/',SkillDeletedView.as_view(), name='delete-skill'),
 ]
