47c44513b2e9605ca7ac5ef6d7c748fc8933efe8
alainkatayi <alainkatayi01@gmail.com>
2025-08-09 12:06:40 +0100

    Création du serializer ExperiencesSerializer dans le serializer de l'application profiles

--- blog/profiles/serializers.py
+++ blog/profiles/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from articles.models import Categories
 from articles.serializers import CategorySerializer
-from .models import Skills
+from .models import Skills, Experiences
 
 class SkillsSerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=True)
@@ -20,3 +20,14 @@ class SkillsSerializer(serializers.ModelSerializer):
         fields = ['id','name','created_at','category','category_id']
         read_only_fields = ['created_at','category']
 
+class ExperiencesSerializer(serializers.ModelSerializer):
+    job_title = serializers.CharField(required= True)
+    entreprise_name = serializers.CharField(required= True)
+    start_date = serializers.DateField(required= True)
+    end_date = serializers.DateField(required=False)
+    current_job = serializers.BooleanField(required= True)
+    description = serializers.CharField(required=True)
+
+    class Meta:
+        model = Experiences
+        fields = ['id','job_title','entreprise_name','start_date','end_date','current_job','description']
