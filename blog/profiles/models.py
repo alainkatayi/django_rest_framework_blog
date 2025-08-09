d96e999a83adae55120741b8b16304eb10453b69
alainkatayi <alainkatayi01@gmail.com>
2025-08-09 11:02:24 +0100

    Ajout des managers SoftDeleteManager et all_objects dans le modèle Skills

--- blog/profiles/models.py
+++ blog/profiles/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+from abstract.base import SoftDeleteModel
+from abstract.manager import SoftDeleteManager
 from articles.models import Categories
 
 # Create your models here.
 
-class Skills(models.Model):
+class Skills(SoftDeleteModel):
     category = models.ForeignKey(Categories, on_delete=models.CASCADE,related_name='skills')
     name= models.TextField()
     created_at = models.DateTimeField(auto_now=True)
+
+    objects = SoftDeleteManager() ## seulement actif
+    all_object = models.Manager() # tout
+
+    def __str__(self):
+        return self.name
