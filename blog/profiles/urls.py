854b1f7e713f72b68f89e812c85ec8dcb2bc2fe1
alainkatayi <alainkatayi01@gmail.com>
2025-08-09 10:21:58 +0100

    Implémentation de la view SkillUpdateView dans le fichier de View de l'application profile, qui fait la mise à jour d'une compétence

--- blog/profiles/urls.py
+++ blog/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateSkillView, SkillsListView
+from .views import CreateSkillView, SkillsListView,SkillUpdateView
 
 urlpatterns = [
     path('create/skill/',CreateSkillView.as_view(), name='create'),
-    path('skills/',SkillsListView.as_view(), name='skills-list')
+    path('skills/',SkillsListView.as_view(), name='skills-list'),
+    path('<int:pk>/update/',SkillUpdateView.as_view(), name='update-skill'),
 ]
