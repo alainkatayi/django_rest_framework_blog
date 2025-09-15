0b4a348568422292d4a865b3a95dc4187330d7a4
alainkatayi <alainkatayi01@gmail.com>
2025-09-15 14:04:19 +0100

    Ajout du filtre par category__name dans views ArticleList de l'application article

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -149,5 +149,6 @@ SIMPLE_JWT = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
